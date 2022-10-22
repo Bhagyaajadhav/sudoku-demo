d87da5a10348c417622b1da856dc20e5ee1b15d6
Bhagyaajadhav <113587988+Bhagyaajadhav@users.noreply.github.com>
2022-10-21 22:20:14 -0700

    Create code.js

---
+++ code.js
@@ -0,0 +1,39 @@
+var arr = [[], [], [], [], [], [], [], [], []]
+
+for (var i = 0; i < 9; i++) {
+	for (var j = 0; j < 9; j++) {
+		arr[i][j] = document.getElementById(i * 9 + j);
+	}
+}
+
+var board = [[], [], [], [], [], [], [], [], []]
+
+function FillBoard(board) {
+	for (var i = 0; i < 9; i++) {
+		for (var j = 0; j < 9; j++) {
+			if (board[i][j] != 0) {
+				arr[i][j].innerText = board[i][j]
+			}
+			else
+				arr[i][j].innerText = ''
+		}
+	}
+}
+
+let GetPuzzle = document.getElementById('GetPuzzle')
+let SolvePuzzle = document.getElementById('SolvePuzzle')
+
+GetPuzzle.onclick = function () {
+	var xhrRequest = new XMLHttpRequest()
+	xhrRequest.onload = function () {
+		var response = JSON.parse(xhrRequest.response);
+		board = response.board;
+		FillBoard(board);
+		console.log(board);
+		//intialization(board);
+
+
+	}
+	xhrRequest.open('get', 'https://sugoku.herokuapp.com/board?difficulty=random')
+	xhrRequest.send()
+}
